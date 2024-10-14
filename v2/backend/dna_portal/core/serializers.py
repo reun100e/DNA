93a9813ef19e96ce69165379ad9557dc53e09b35
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-14 11:13:53 +0530

    Add core serializers

---
+++ v2/backend/dna_portal/core/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'phone_number', 'phone_verified', 'email_verified']

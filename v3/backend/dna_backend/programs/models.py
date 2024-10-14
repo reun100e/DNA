b62b544a55f8fad1dac98c8bc977220b8e6821ad
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-14 13:35:42 +0530

    Add programs/models

--- v3/backend/dna_backend/programs/models.py
+++ v3/backend/dna_backend/programs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    description = models.TextField()
+    event_date = models.DateField()

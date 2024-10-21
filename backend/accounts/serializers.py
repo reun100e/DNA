17005b24cc696ac08ce543463f22e10b15c28316
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-21 12:43:38 +0530

    Mandate first and last name for user registration

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -13,15 +13,24 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
     password = serializers.CharField(write_only=True, min_length=8)
+    first_name = serializers.CharField(required=True, max_length=30)
+    last_name = serializers.CharField(required=True, max_length=30)
 
     def validate_phone_number(self, value):
         if not re.match(r'^\+?\d{10,15}$', value):
             raise ValidationError("Invalid phone number format.")
         return value
 
+    def validate(self, data):
+        if not data.get('first_name').strip():
+            raise ValidationError({"first_name": "First name cannot be empty."})
+        if not data.get('last_name').strip():
+            raise ValidationError({"last_name": "Last name cannot be empty."})
+        return data
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'phone_number', 'dna_id']
+        fields = ['username', 'email', 'password', 'first_name', 'last_name','phone_number', 'dna_id']
         read_only_fields = ['dna_id']  # Prevent users from changing dna_id
 
 
@@ -30,7 +39,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             username=validated_data['username'],
             email=validated_data['email'],
             phone_number=validated_data.get('phone_number'),
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         return user
 
@@ -68,7 +79,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # User can see all these fields
-        fields = ['username', 'email', 'phone_number', 'is_phone_verified', 'is_email_verified', 'dna_id']
+        fields = ['username','first_name', 'last_name', 'email', 'phone_number', 'is_phone_verified', 'is_email_verified', 'dna_id']
         # User cannot edit these fields
         read_only_fields = ['username', 'is_phone_verified', 'is_email_verified', 'dna_id']
 
@@ -76,5 +87,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
         # Only allow updating email and phone_number
         instance.email = validated_data.get('email', instance.email)
         instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
         instance.save()
         return instance

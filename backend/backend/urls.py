cf4e5a67cbb7891645241d61a95f8e8a7ed75eaf
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-10-31 16:33:26 +0530

    Add enpdoin urls for badges, history, payments, registrations

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,5 +5,9 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
+    path("api/v1/badges/", include("badges.urls")),
+    path("api/v1/history/", include("history.urls")),
+    path("api/v1/payments/", include("payments.urls")),
     path("api/v1/programs/", include("programs.urls")),
+    path("api/v1/registrations/", include("registrations.urls")),
 ]

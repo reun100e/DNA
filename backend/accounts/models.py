533426ddf0106206041cd5a8e34cefb3d64d2448
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-11-12 17:38:22 +0530

    Add user profile model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 import uuid
 import base64
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
 
@@ -13,22 +13,29 @@ class User(AbstractUser):
     is_phone_verified = models.BooleanField(default=False)
     is_email_verified = models.BooleanField(default=False)
 
-    dna_id = models.CharField(max_length=20, unique=True, blank=True, editable=False, help_text="Unique DNA identifier.")
+    dna_id = models.CharField(
+        max_length=20,
+        unique=True,
+        blank=True,
+        editable=False,
+        help_text="Unique DNA identifier.",
+    )
 
     def __str__(self):
         return self.username
 
+
 # Function to generate Base32-encoded UUID
 def generate_dna_id():
     uuid_bytes = uuid.uuid4().bytes
-    return base64.b32encode(uuid_bytes).decode('utf-8').strip('=')
+    return base64.b32encode(uuid_bytes).decode("utf-8").strip("=")
 
 
 # Signal to assign the unique ID before saving the user
 @receiver(pre_save, sender=User)
 def set_unique_user_id(sender, instance, **kwargs):
     if not instance.dna_id:
-        #The while loop guarantees that the generated ID is unique by checking against the database.
+        # The while loop guarantees that the generated ID is unique by checking against the database.
         while True:
             new_id = generate_dna_id()
             if not User.objects.filter(dna_id=new_id).exists():
@@ -46,4 +53,34 @@ def reset_verification_on_change(sender, instance, **kwargs):
         if instance.phone_number != old_user.phone_number:
             instance.is_phone_verified = False
 
-#Add profile model with sex, bio, college, etc
+
+# User profile model additions in future: sex, college, etc
+
+from django.conf import settings
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    bio = models.TextField(max_length=500, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="private/profile_pictures/", blank=True, null=True
+    )
+    # Add other fields as needed
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
+# Signal to create UserProfile when a new User is created
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+# Signal to save the UserProfile whenever the User is saved
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

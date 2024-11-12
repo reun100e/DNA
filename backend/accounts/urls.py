5bf60cf0f4c5cd8d5f0ed20e60879a20807d2c74
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-11-12 17:43:33 +0530

    updated accounts endpoints

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,18 +1,21 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RefreshTokenView, UserProfileView, LogoutView
+from .views import RegisterView, LoginView, RefreshTokenView, UserDetailView, LogoutView, WhoAmIView, private_media
 
 urlpatterns = [
     # User registration
     path("users/register/", RegisterView.as_view(), name="user-register"),
 
-    # User login with access abd refresh token in Http-only cookies
+    # User login with access and refresh token in Http-only cookies
     path("users/login/", LoginView.as_view(), name="user-login"),
 
     # Get new access token in Http-only cookies
     path("users/refresh/", RefreshTokenView.as_view(), name="user-token-refresh"),
 
-    # User Profile
-    path("me/", UserProfileView.as_view(), name="user-profile"),
+    # Get active User info
+    path("users/me/", WhoAmIView.as_view(), name="user-profile"),
+
+    # Get and Patch User Profile
+    path("users/profile/", UserDetailView.as_view(), name="user-profile"),
 
     # Logout
     path("users/logout/", LogoutView.as_view(), name="user-logout"),

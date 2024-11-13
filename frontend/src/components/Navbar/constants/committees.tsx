e5c319594149905e08430f5b76209a57293b4fd0
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-11-13 23:09:11 +0530

    Update Navbar constants

---
+++ frontend/src/components/Navbar/constants/committees.tsx
@@ -0,0 +1,14 @@
+export const committees: { title: string; link: string; description: string }[] = [
+  {
+    title: "The Great Homoeopathic Assembly",
+    link: "/great-homoeopathic-assembly",
+    description:
+      "Dive into the legacy of homeopathic legends. Assume the role of a revered homeopathic stalwart and bring the art and science of homoeopathy to life. This is your chance to stand out, showcase your understanding, and bring your perspective to the homeopathic world.",
+  },
+  {
+    title: "World Health Organization Assembly",
+    link: "/who-assembly",
+    description:
+      "Become a champion for global health solutions. In the WHO Assembly, you’ll take on the role of a healthcare leader, tackling the most pressing health issues of our time and working towards a healthier future for all.",
+  },
+];
---
+++ frontend/src/components/Navbar/constants/diimun.tsx
@@ -0,0 +1,8 @@
+export const diimun: { title: string; link: string; description: string }[] = [
+  {
+    title: "DIIMUN: Doctors Integrated International Model United Nations",
+    link: "/diimun",
+    description:
+      "Learn diplomacy and international health policy by participating in DIIMUN, a unique Model United Nations experience tailored for medical professionals. Enhance your leadership and negotiation skills while discussing global healthcare challenges.",
+  },
+];

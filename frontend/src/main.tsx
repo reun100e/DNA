054ae6ca786956de369f99e026455c249f700f5b
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-11-14 23:12:37 +0530

    Add react-tostify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
+    <ToastContainer toastStyle={{ backgroundColor: "#d9daf2" }} position="top-right" autoClose={3000} hideProgressBar />
   </StrictMode>
 );

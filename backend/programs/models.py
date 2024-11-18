9416db78243a81511b3dcfc4a78a1e0610411ec9
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2024-11-18 13:21:11 +0530

    Refactor programs app

--- backend/programs/models.py
+++ backend/programs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Program(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -7,12 +8,14 @@ class Program(models.Model):
     def __str__(self):
         return self.name
 
+
 class Event(models.Model):
     name = models.CharField(max_length=100)
-    program = models.ForeignKey(Program, on_delete=models.CASCADE, related_name='events')
+    program = models.ForeignKey(
+        Program, on_delete=models.CASCADE, related_name="events"
+    )
     description = models.TextField()
     date = models.DateField()
 
     def __str__(self):
         return f"{self.name} on {self.date} under {self.program.name}"
-
--- backend/programs/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Registration
-from programs.models import Event
-
-class RegistrationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Registration
-        fields = ['id', 'user', 'event', 'registration_date']
-        read_only_fields = ['id', 'registration_date']
